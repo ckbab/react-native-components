421ad25221c973f1f3a7ba866569ebd4cf0810a9
Marcus Christiansson <christiansson.marcus@gmail.com>
2022-05-03 22:13:40 +0200

    Update MessageContainer.js

--- components/AppContainer/MessageContainer.js
+++ components/AppContainer/MessageContainer.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import FlashMessage from "react-native-flash-message";
+import { useScreen } from "../../hooks";
 import { shadow4 } from "../../styles";
 
 export default function MessageContainer() {
+  const screen = useScreen();
   return (
     <FlashMessage
-      position="top"
       style={styles.flashContainer}
       titleStyle={[styles.flashTitle, { fontFamily: "bold" || null }]}
       textStyle={[styles.flashText, { fontFamily: "regular" || null }]}
+      position="top"
+      statusBarHeight={screen.statusBarHeight}
     />
   );
 }
